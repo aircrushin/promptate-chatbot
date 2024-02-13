acc28f2e25c26912a09da7ec8c867f137fcdd3d6
UltraV <1216564567@qq.com>
2024-02-13 23:54:58 +0800

    add config

--- Chatbot.py
+++ Chatbot.py
@@ -3,21 +3,20 @@ import streamlit as st
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    "[获取API Key](https://platform.openai.com/account/api-keys)"
+    "[返回Promptate](https://promptate.vercel.app/)"
 
-st.title("💬 Chatbot")
+st.title("💬 Promptate 聊天机器人")
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "您好！我是Promptate的聊天机器人，有什么可以帮助您的?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
     if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
+        st.info("请输入您的API KEY")
         st.stop()
 
     client = OpenAI(api_key=openai_api_key)
